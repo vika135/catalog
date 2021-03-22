f67677085ced5ef4bc1d0a684922676bea7f3c7c
vika135 <ptizza.music@yandex.ru>
2021-03-22 19:25:32 +0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'catalog-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'catalog';
+}
